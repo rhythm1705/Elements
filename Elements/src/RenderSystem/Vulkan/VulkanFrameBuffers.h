f4bd52801880f15d621cec71e8e61ab097db3091
Rhythm Goel <rhythm1705@outlook.com>
2020-07-07 23:00:56 -0400

    Create vulkan framebuffers

---
+++ Elements/src/RenderSystem/Vulkan/VulkanFrameBuffers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "RenderSystem/Vulkan/VulkanImageView.h"
+
+namespace Elements {
+
+class VulkanFramebuffers {
+  public:
+    VulkanFramebuffers(VulkanImageViews *vulkanImageViewsj, vk::Extent2D extent);
+    ~VulkanFramebuffers();
+
+    std::vector<vk::Framebuffer> getFrameBuffers() { return framebuffers; }
+
+  private:
+    std::vector<vk::Framebuffer> framebuffers;
+};
+} // namespace Elements
